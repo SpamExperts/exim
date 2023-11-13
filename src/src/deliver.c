31ea3dde868e9b2d55da1890a760e4455042c80e
cristianpier <cristian.piersinaru@n-able.com>
2023-11-13 10:19:30 +0200

    Add extra condition to prevent exim segfaults in `print_dsn_diagnosti…

--- src/src/deliver.c
+++ src/src/deliver.c
@@ -5393,6 +5393,9 @@ Returns:       nothing
 static void
 print_dsn_diagnostic_code(const address_item *addr, FILE *f)
 {
+
+if (addr == NULL) return;
+
 uschar * s = testflag(addr, af_pass_message) ? addr->message : NULL;
 unsigned cnt;
 
