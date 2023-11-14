b48f00fd4ba91dda498c3e97290dc32aa60e19ad
cristianpier <cristian.piersinaru@n-able.com>
2023-11-14 13:45:32 +0200

    remove extra line

--- src/src/deliver.c
+++ src/src/deliver.c
@@ -5397,7 +5397,6 @@ print_dsn_diagnostic_code(const address_item *addr, FILE *f)
 if (addr == NULL) return;
 
 uschar * s;
-
 unsigned cnt;
 DEBUG(D_deliver)
   debug_printf("DSN Diagnostic-Code: pass_message=%s, message=%s, user_message=%s\n", testflag(addr, af_pass_message), addr->message, addr->user_message);
