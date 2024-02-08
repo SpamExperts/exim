b838362976931f1b286c0d32d22e799c5d747b6e
Vlad Georgescu <Vlad.Georgescu@n-able.com>
2024-02-08 17:32:28 +0200

    test

--- src/src/receive.c
+++ src/src/receive.c
@@ -1733,7 +1733,9 @@ header_line *subject_header = NULL;
 header_line *msgid_header = NULL;
 header_line *received_header;
 BOOL msgid_header_newly_created = FALSE;
-
+#ifdef SUPPORT_DMARC
+int dmarc_up = 0;
+#endif
 /* Variables for use when building the Received: header. */
 
 uschar *timestamp;
@@ -1807,7 +1809,7 @@ if (smtp_input && !smtp_batched_input && !f.dkim_disable_verify)
 #endif
 
 #ifdef SUPPORT_DMARC
-if (sender_host_address) dmarc_init();	/* initialize libopendmarc */
+dmarc_up = dmarc_init();	/* initialize libopendmarc */
 #endif
 
 /* In SMTP sessions we may receive several messages in one connection. Before
@@ -3577,7 +3579,7 @@ else
 #endif /* WITH_CONTENT_SCAN */
 
 #ifdef SUPPORT_DMARC
-    dmarc_store_data(from_header);
+    dmarc_up = dmarc_store_data(from_header);
 #endif
 
 #ifndef DISABLE_PRDR
