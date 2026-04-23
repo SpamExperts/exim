7a33581e1602494e0e74e3bd1c52a7a286775647
Victor Baranga <victor.baranga@n-able.com>
2026-04-23 16:38:48 +0300

    Move print_dsn_diagnostic_code inside loop

--- src/src/deliver.c
+++ src/src/deliver.c
@@ -5839,7 +5839,7 @@ wording. */
 	"The following text was generated during the delivery "
 	"attempt%s:\n", (filecount > 1)? "s" : "");
 
-    for (addr = msgchain; addr; addr = nextaddr)
+    for (address_item * addr = msgchain; addr; addr = nextaddr)
       {
       FILE *fm;
       address_item *topaddr = addr;
@@ -5874,8 +5874,8 @@ wording. */
       nextaddr = addr->next;
       addr->next = handled_addr;
       handled_addr = topaddr;
-      }
       print_dsn_diagnostic_code(addr, fp);
+      }
     fputc('\n', fp);
     }
 
